80862411e9251ee66ca96431f80715b26ddf084a
Hunsh <327494410@qq.com>
2016-08-18 10:31:35 +0800

    xxx
    yyy

--- open-bbs.common/src/main/java/com/openbbs/exception/ErrorCode.java
+++ open-bbs.common/src/main/java/com/openbbs/exception/ErrorCode.java
@@ -6,7 +6,31 @@ package com.openbbs.exception;
  * @date 2016年8月12日
  *
  */
-public interface ErrorCode {
+public enum ErrorCode {
+	;
+	
+	private long code;
+	
+	private String desciption;
+	
+	private Throwable cause;
+	
+	private ErrorCode(long code, String description, Throwable cause){
+		this.code = code;
+		this.desciption = description;
+		this.cause = cause;
+	}
+	
+	public long getCode() {
+		return code;
+	}
 
-	String SUCCESS = "0";
+	public String getDesciption() {
+		return desciption;
+	}
+
+	public Throwable getCause() {
+		return cause;
+	}
+	
 }
